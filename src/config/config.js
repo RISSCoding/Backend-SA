591b1cc48b1691174bd390730130828b76adf24a
RISSCoding <farisfebrian0602@gmail.com>
2024-08-23 09:17:15 +0700

    add db

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'your-secret-key',
+    jwtSecret: process.env.JWT_SECRET || '24578563',
   };
   
   export default config;
